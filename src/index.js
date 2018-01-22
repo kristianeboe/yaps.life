b411760ba7663774c433a4e4e0a7baa7f43db9c1
Kristian Elset Bø <kristian.e.boe@gmail.com>
2018-01-22 13:11:19 +0100

    moved routerWrapper to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import RouterWrapper from './RouterWrapper';
+// import RouterWrapper from './RouterWrapper';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+import App from './App'
+import User from './User'
+
+const RouterWrapper = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={App}/>
+      <Route path="/user" component={User} />
+    </div>
+  </Router>
+)
 
 ReactDOM.render(<RouterWrapper />, document.getElementById('root'));
 registerServiceWorker();
