002664ea286931cfc2e1a4fa92603a0e8653d1e9
Kristian Elset Bø <kristian.e.boe@gmail.com>
2018-01-23 14:55:47 +0100

    Added google places autocomplete

--- src/Register.js
+++ src/Register.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Form, Segment, Container, Dropdown, TextArea, Message } from 'semantic-ui-react'
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button/Button';
 import firebase from './firebase'
+import PlacesAutocomplete, { geocodeByAddress, getLatLng, geocodeByPlaceId } from 'react-places-autocomplete'
 
 const gender_options = [
   { key: 'm', text: 'Male', value: 'male' },
@@ -58,19 +59,73 @@ class Register extends Component {
       max_rent: '',
       other_requests: '',
       tos_checkbox: '',
-      loading: false,
+      form_loading: false,
       form_success: false,
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.extractUserFromState = this.extractUserFromState.bind(this);
   }
 
   handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
+  handleSelect(workplace) {
+    this.setState({
+      workplace,
+      loading: true,
+    })
+
+    geocodeByAddress(workplace)
+      .then(results => getLatLng(results[0]))
+      .then(({ lat, lng }) => {
+        console.log('Success Yay', { lat, lng })
+        this.setState({
+          geocodeResults: this.renderGeocodeSuccess(lat, lng),
+          loading: false,
+        })
+      })
+      .catch(error => {
+        console.log('Oh no!', error)
+        this.setState({
+          geocodeResults: this.renderGeocodeFailure(error),
+          loading: false,
+        })
+      })
+
+  }
+
+  extractUserFromState() {
+
+    let user = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email,
+      gender: this.state.gender,
+      age: this.state.age,
+      field_of_study: this.state.field_of_study,
+      university: this.state.university,
+      workplace: this.state.workplace,
+      field_of_work: this.state.field_of_work,
+      linkedIn_id: this.state.linkedIn_id,
+      preferences_apartment: this.state.preferences_apartment,
+      preferences_roommates: this.state.preferences_roommates,
+      max_rent: this.state.max_rent,
+      other_requests: this.state.other_requests,
+      geocodeResults: this.state.geocodeResults,
+    }
+
+    return user
+  }
+
   handleSubmit = (event) => {
     this.setState({ loading: true })
-    const user = this.state
+    let user = this.extractUserFromState()
+
+    // if (firebase.auth().currentUser) {
+    //   user['google_uid'] = firebase.auth().currentUser.uid
+    // }
 
     const collection = firebase.firestore().collection('users');
     collection.add(user).then(() => {
@@ -90,60 +145,108 @@ class Register extends Component {
         max_rent: '',
         other_requests: '',
         tos_checkbox: '',
-        loading: false,
+        form_loading: false,
         form_success: true
       })
     });
   }
 
   render() {
-    const {
-      first_name,
-      last_name,
-      email,
-      gender,
-      age,
-      field_of_study,
-      university,
-      workplace,
-      field_of_work,
-      preferences_apartment,
-      preferences_roommates,
-      linkedIn_id,
-      max_rent,
-      other_requests,
-      tos_checkbox,
-      loading,
-      form_success } = this.state
+    // const {
+    //   first_name,
+    //   last_name,
+    //   email,
+    //   gender,
+    //   age,
+    //   field_of_study,
+    //   university,
+    //   workplace,
+    //   field_of_work,
+    //   preferences_apartment,
+    //   preferences_roommates,
+    //   linkedIn_id,
+    //   max_rent,
+    //   other_requests,
+    //   tos_checkbox,
+    //   form_loading,
+    //   form_success } = this.state
+
+    const inputProps = {
+      name: 'workplace',
+      placeholder: 'Where are you going to work?',
+      value: this.state.workplace,
+      onChange: (workplace) => {
+        this.setState({
+          workplace,
+          geocodeResults: null,
+        })
+      },
+    }
+
+    const Footer = () => (
+      <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '4px', }}>
+        <div>
+          <img
+            src={require('./powered_by_google_default.png')}
+            style={{ display: 'inline-block', width: '150px' }}
+          />
+        </div>
+      </div>
+    )
+
+    const shouldFetchSuggestions = ({ value }) => value.length > 2
 
     return (
       <Segment inverted vertical id="register-section">
         <Container>
-          <Form onSubmit={this.handleSubmit} inverted loading={loading} success={form_success}>
+          <Form onSubmit={this.handleSubmit} inverted loading={this.state.form_loading} success={this.state.form_success}>
             <Form.Group widths='equal'>
-              <Form.Input required label='First Name' placeholder='First Name' name='first_name' value={first_name} onChange={this.handleChange} />
-              <Form.Input required label='Last Name' placeholder='Last Name' name='last_name' value={last_name} onChange={this.handleChange} />
+              <Form.Input required label='First Name' placeholder='First Name' name='first_name' value={this.state.first_name} onChange={this.handleChange} />
+              <Form.Input required label='Last Name' placeholder='Last Name' name='last_name' value={this.state.last_name} onChange={this.handleChange} />
             </Form.Group>
-            <Form.Input required label='Email' placeholder='Email' name='email' value={email} onChange={this.handleChange} />
+            <Form.Input required label='Email' placeholder='Email' name='email' value={this.state.email} onChange={this.handleChange} />
             <Form.Group widths='equal'>
-              <Form.Select required label='Gender' placeholder='Gender' name='gender' value={gender} options={gender_options} onChange={this.handleChange} />
-              <Form.Input required label='Age' placeholder='Age' name='age' value={age} onChange={this.handleChange} />
+              <Form.Select required label='Gender' placeholder='Gender' name='gender' value={this.state.gender} options={gender_options} onChange={this.handleChange} />
+              <Form.Input required label='Age' placeholder='Age' name='age' value={this.state.age} onChange={this.handleChange} />
             </Form.Group>
             <Form.Group widths='equal'>
-              <Form.Input label='Field of study' placeholder='Field of study' name='field_of_study' value={field_of_study} onChange={this.handleChange} />
-              <Form.Input required label='University' placeholder='University' name='university' value={university} onChange={this.handleChange} />
+              <Form.Input label='Field of study' placeholder='Field of study' name='field_of_study' value={this.state.field_of_study} onChange={this.handleChange} />
+              <Form.Input required label='University' placeholder='University' name='university' value={this.state.university} onChange={this.handleChange} />
             </Form.Group>
-            <Form.Input label='Address of workplace' placeholder='Where are you going to work?' name='workplace' value={workplace} onChange={this.handleChange} />
-            <label>Type of work</label>
-            <Dropdown placeholder='Type of work' name='field_of_work' value={field_of_work} fluid multiple search selection options={type_of_work_options} onChange={this.handleChange} />
-            <label>Interests</label>
-            <Dropdown label='Interests' placeholder='Interests' name='preferences_apartment' value={preferences_apartment} fluid multiple search selection options={interests_options} onChange={this.handleChange} />
-            <label>Roommate preferences</label>
-            <Dropdown label='Roommates' placeholder='Roommates' name='preferences_roommates' value={preferences_roommates} fluid multiple search selection options={preferences_roommates_options} onChange={this.handleChange} />
-            <Form.Input required label='LinkedIn id' placeholder='LinkedIn id' name='linkedIn_id' value={linkedIn_id} onChange={this.handleChange} />
-            <Form.Input label='Max rent' placeholder='Max rent' name='max_rent' value={max_rent} onChange={this.handleChange} />
-            <TextArea label='Other notes' placeholder='Other requests, notes or concerns?' name='other_requests' value={other_requests} onChange={this.handleChange} />
-            <Form.Checkbox required label='I agree to the Terms and Conditions' name='tos_checkbox' value={tos_checkbox} onChange={this.handleChange} />
+
+
+            <Form.Field required>
+              <label>Workplace</label>
+              <PlacesAutocomplete
+                styles={{ root: { zIndex: 50 } }}
+                inputProps={inputProps}
+                renderFooter={Footer}
+                onEnterKeyDown={this.handleSelect}
+                shouldFetchSuggestions={shouldFetchSuggestions}
+                highlightFirstSuggestion
+                options={{
+                  types: ['establishment']
+                }}
+              />
+            </Form.Field>
+
+
+            <Form.Field>
+              <label>Type of work</label>
+              <Dropdown placeholder='Type of work' name='field_of_work' value={this.state.field_of_work} fluid multiple search selection options={type_of_work_options} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <label>Interests</label>
+              <Dropdown label='Interests' placeholder='Interests' name='preferences_apartment' value={this.state.preferences_apartment} fluid multiple search selection options={interests_options} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <label>Roommate preferences</label>
+              <Dropdown label='Roommates' placeholder='Roommates' name='preferences_roommates' value={this.state.preferences_roommates} fluid multiple search selection options={preferences_roommates_options} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Input required label='LinkedIn id' placeholder='LinkedIn id' name='linkedIn_id' value={this.state.linkedIn_id} onChange={this.handleChange} />
+            <Form.Input label='Max rent' placeholder='Max rent' name='max_rent' value={this.state.max_rent} onChange={this.handleChange} />
+            <TextArea label='Other notes' placeholder='Other requests, notes or concerns?' name='other_requests' value={this.state.other_requests} onChange={this.handleChange} />
+            <Form.Checkbox required label='I agree to the Terms and Conditions' name='tos_checkbox' value={this.state.tos_checkbox} onChange={this.handleChange} />
             <Button type="submit" >Submit</Button>
             <Message
               success
